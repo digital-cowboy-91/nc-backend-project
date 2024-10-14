d68cf5030e9cc147885a9bacebad0d28fa026839
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-10-14 12:45:49 +0100

    adds get method for topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,11 @@
 const request = require("supertest");
 const app = require("../app.js");
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
 
 describe("/api/healthCheck", () => {
   describe("GET", () => {
@@ -9,6 +15,30 @@ describe("/api/healthCheck", () => {
         .expect(200)
         .then((res) => {
           expect(res.body.msg).toBe("Alive!");
+        });
+    });
+  });
+});
+
+describe("/api/topics", () => {
+  describe("GET", () => {
+    test("Responds with 200 and array of topics", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then((res) => {
+          const topics = res.body.topics;
+
+          expect(topics.length).not.toBe(0);
+
+          topics.forEach((topic) => {
+            expect(topic).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
         });
     });
   });
---
+++ api/models/readTopics.model.js
@@ -0,0 +1,7 @@
+const db = require("../../db/connection.js");
+
+function readTopics() {
+  return db.query(`SELECT * FROM topics`).then((data) => data.rows);
+}
+
+module.exports = readTopics;
