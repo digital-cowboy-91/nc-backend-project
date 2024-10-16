66c889ccdf07fadc2ed63a36cac92c2b631bf0aa
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-10-16 13:58:35 +0100

    configures prod db

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,23 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === "production") {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL not set");
+  }
+
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
