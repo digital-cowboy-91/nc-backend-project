7e0eae14434e6107e3fdb338bcd02142f9c5af02
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-10-17 11:34:53 +0100

    adds get method to retrieve users by username

--- api/users/router.js
+++ api/users/router.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const getUsers = require("./controllers/getUsers.controller");
+const getUserByUsername = require("./controllers/getUserByUsername.controller");
 
 router.route("/api/users").get(getUsers);
+
+router.route("/api/users/:username").get(getUserByUsername);
 
 module.exports = router;
